80a32457462fa008faed2fdb6a5aa8d70fb55085
Victor Hugo <victorblog410@gmail.com>
2021-06-19 15:01:20 -0300

    integrating PostgreSQL with docker

--- app_ts/settings.py
+++ app_ts/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'app_ts.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE"),
+        "NAME": os.environ.get("SQL_DATABASE"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
