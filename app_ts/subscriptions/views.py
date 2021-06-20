aab2d2145ff86298901580a54dbb4b513171d696
Victor Hugo <victorblog410@gmail.com>
2021-06-19 22:54:15 -0300

    making the view more elegante.

--- app_ts/subscriptions/views.py
+++ app_ts/subscriptions/views.py
@@ -7,20 +7,25 @@ from django.db.models import Q
 
 def search(request):
     query = request.GET.get('query', '')
-    subscription = Subscription.objects.filter(Q(name__icontains=query)
-                                               | Q(cpf__icontains=query)
-                                               | Q(email__icontains=query)
-                                               | Q(phone=query))
+    subscription = Subscription.objects.filter(
+        Q(name__icontains=query)
+        | Q(cpf__icontains=query)
+        | Q(email__icontains=query)
+        | Q(phone=query)
+    )
     return render(
-           request,
-           'subscription/subscription_list.html',
-           {'subscription': subscription, 'query': query})
+        request,
+        'subscription/subscription_list.html',
+        {'subscription': subscription, 'query': query})
 
 
 def subscription_list(request):
     subscription = Subscription.objects.all()[0:8]
-    return render(request, 'subscription/subscription_list.html',
-                  {'subscription': subscription})
+    return render(
+        request,
+        'subscription/subscription_list.html',
+        {'subscription': subscription},
+    )
 
 
 def added_subscription(request):
@@ -29,16 +34,19 @@ def added_subscription(request):
 
         if form.is_valid():
             obj = form.save()
-            messages.success(request,
-                             'Palestra registrada com sucesso')
+            messages.success(
+                request,
+                'Palestra registrada com sucesso')
 
             return redirect('subscription:subscription-detail', obj.pk)
     else:
         form = SubscriptionForm()
 
-    return render(request,
-                  'subscription/subscription_added.html',
-                  {'form': form})
+    return render(
+        request,
+        'subscription/subscription_added.html',
+        {'form': form},
+    )
 
 
 def update_subscription(request, pk):
@@ -50,17 +58,20 @@ def update_subscription(request, pk):
 
         return redirect("subscription:subscription-update', obj.pk")
 
-    return render(request,
-                  "subscription/subscription_added.html",
-                  {"form": form})
+    return render(
+        request,
+        "subscription/subscription_added.html",
+        {"form": form})
 
 
 def subscription_detail(request, pk):
     template_name = 'subscription/detail_subscription.html'
     obj = Subscription.objects.get(pk=pk)
-    return render(request,
-                  template_name,
-                  {'object': obj})
+    return render(
+        request,
+        template_name,
+        {'object': obj},
+    )
 
 
 def delete_subscription(request, pk):
